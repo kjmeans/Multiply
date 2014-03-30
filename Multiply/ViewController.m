54175ad61381e6c73ed31cbbf27fafe4a3ef7e02
kjmeans <kellemeans@gmail.com>
2014-03-30 13:30:56 -0500

    Multiply Project base

--- Multiply/ViewController.m
+++ Multiply/ViewController.m
@@ -10,14 +10,47 @@
 
 @interface ViewController ()
 
+@property (strong, nonatomic) IBOutlet UITextField *myNumber;
+@property (strong, nonatomic) IBOutlet UILabel *myMultiplier;
+@property (weak, nonatomic) IBOutlet UILabel *myAnswer;
+
+
+@property (weak, nonatomic) IBOutlet UISlider *myMultiplierSlider;
+@property (strong, nonatomic) IBOutlet UIButton *resignButtonView;
+
 @end
 
 @implementation ViewController
+
+-(IBAction)adjustMyMultiplier:(UISlider*) sender {
+    self.myMultiplier.text = [NSString stringWithFormat:@"%d", (int)self.myMultiplierSlider.value];
+}
+
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (IBAction)onCalculateButtonPressed:(id)sender {
+
+    int number = [self.myNumber.text integerValue];
+    int secondNumber = [self.myMultiplier.text integerValue];
+    int result = number * secondNumber;
+                  
+    self.myAnswer.text = [NSString stringWithFormat:@"%d", result];
+    
+    if (result > 20) {
+        self.view.backgroundColor = [UIColor greenColor];
+    }
+        
+    }
+    
+-(IBAction)resignKeyboard:(id)sender{
+    [self.view endEditing:YES];
+    
 }
 
 - (void)didReceiveMemoryWarning
